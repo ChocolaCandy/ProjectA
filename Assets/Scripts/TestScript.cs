950a8eb55c7f898d3891393778d8449e7ada2e44
ChocolaCandy <ChocolaCandy@users.noreply.github.com>
2024-05-06 23:53:30 +0900

    20240506 Camera Method Partially completed
    ZoomMethod completed
    FocusPlayer completed

--- Assets/Scripts/TestScript.cs
+++ Assets/Scripts/TestScript.cs
@@ -1,13 +1,66 @@
+using System;
 using System.Collections;
-using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class TestScript : MonoBehaviour
 {
     public GameObject a;
-
-    private void Update()
+    [SerializeField] private float lerpTime = 1f;
+    private float currentTime;
+    float smoothTime = 0.12f;
+    int i = 0;
+    private float startTime;
+    public Camera ace;
+    public float after;
+    private void Start()
     {
-        transform.RotateAround(a.transform.position, Vector3.up, 20 * Time.deltaTime);
+        startTime = Time.time;
     }
+
+    private void LateUpdate()
+    {
+        if (transform.position == a.transform.position)
+        {
+            Debug.Log($"{i} : ÀÏÄ¡");
+            return;
+        }
+        float t = Time.deltaTime;
+        ScrollUpDown();
+        currentTime += Time.deltaTime * smoothTime;
+        Debug.Log(currentTime);
+        if (currentTime >= lerpTime)
+        {
+            //t = lerpTime;
+        }
+
+        i++;
+        float fracComplete = (Time.time - startTime) / 1;
+        Debug.Log(fracComplete);
+        transform.position = Vector3.Lerp(transform.position, a.transform.position + RotateAround(), fracComplete);
+        ace.fieldOfView = Mathf.Lerp(ace.fieldOfView, after, Time.deltaTime);
+    }
+    public Vector3 RotateAround()
+    {
+
+        float xMouseInput = Input.GetAxis("Mouse X") * 500 * Time.deltaTime;
+        float yMouseInput = Input.GetAxis("Mouse Y") * 500 * Time.deltaTime;
+
+        Vector3 ad = transform.position - a.transform.position;
+        Quaternion quaternion = Quaternion.AngleAxis(xMouseInput, a.transform.up);
+        Quaternion quaternion1 = Quaternion.AngleAxis(-yMouseInput, a.transform.right);
+        Vector3 vector2 = quaternion * quaternion1 * ad;
+        return vector2;
+    }
+    private void ScrollUpDown()
+    {
+        float wheelInput = Input.GetAxis("Mouse ScrollWheel");
+        if (wheelInput == 0.0f)
+        {
+            return;
+        }
+        float afterRadius = Mathf.Clamp(ace.fieldOfView + -(wheelInput * 100), 20, 60);
+        after = afterRadius;
+    }
+
 }
--- Assets/Scripts/TownCameraController.cs
+++ Assets/Scripts/TownCameraController.cs
@@ -1,40 +1,53 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.SceneManagement;
-using UnityEngine.EventSystems;
-
-using UnityEngine.UI;
-using static UnityEngine.GraphicsBuffer;
-using System;
-using UnityEngine.UIElements;
-using static Unity.IO.LowLevel.Unsafe.AsyncReadManagerMetrics;
 
 [RequireComponent (typeof (Camera))]
 public class MainCameraController : MonoBehaviour
 {
-    [Header("Setting value")]
-    public float ScrollSensitivity = 10.0f;
-    //private
-    private Vector3 _endPoint = Vector3.zero;
-    private float _radius = 0.0f;
-
-    [Header("Default value")]
-    [SerializeField]
-    [Tooltip("Init distance between player and camera")]
-    private float _initDistance = 2.0f;
-    [SerializeField]
-    [Tooltip("Default distance between player and camera")]
-    private float _defaultDistance = 5.0f;
-
     [Header("Object")]
     [SerializeField]
     [Tooltip("Transform to focus")]
     private Transform _focusObject = null;
     [SerializeField]
-    [Tooltip("Main Camera Component")]
+    [Tooltip("Main camera component")]
     private Camera _camera = null;
+
+    [Header("Scroll setting")]
+    [SerializeField]
+    [Tooltip("Sensitivity of scroll")]
+    private float ScrollSensitivity = 100.0f;
+    [SerializeField]
+    [Tooltip("Min value of camera FOV")]
+    private float MinFOV = 20.0f;
+    [SerializeField]
+    [Tooltip("Max value of camera FOV")]
+    private float MaxFOV = 60.0f;
+
+    [Header("Mouse setting")]
+    [SerializeField]
+    private float MouseSensitivity = 100.0f;
+
+    [Header("Default value")]
+    [SerializeField]
+    [Tooltip("Init distance between camera and player")]
+    private float _cameraDistance = 5.0f;
+    [SerializeField]
+    [Tooltip("Init value of camera FOV")]
+    private float _initFOV = 20.0f;
+    [SerializeField]
+    [Tooltip("Default value of EndPoint FOV")]
+    private float _defaultFOV = 40.0f;
+
+    [Header("Debug value")]
+    [SerializeField]
+    [Tooltip("Camera vector from player")]
+    private Vector3 _endPointPosition = Vector3.zero;
+    [SerializeField]
+    [Tooltip("Camera Fov")]
+    public float _endPointFOV = 0.0f;
 
     private void Start()
     {
@@ -47,20 +60,18 @@ public class MainCameraController : MonoBehaviour
         }
         InitCameraPosition();
     }
-
     private void LateUpdate()
     {
-        Scroll();
+        //줌인/줌아웃
+        ZoomInOut();
 
         //회전
-        //RotateAround();
-
-        //이동
+        dd();
 
         //카메라 트랜스폼 변경
-        transform.position = Vector3.Lerp(transform.position, _focusObject.position + _endPoint, Time.deltaTime);
+        _camera.fieldOfView = Mathf.Lerp(_camera.fieldOfView, _endPointFOV, Time.deltaTime * 1.5f);
+        transform.position = _focusObject.position + _endPointPosition;
         transform.LookAt(_focusObject);
-        _radius = (transform.position - _focusObject.position).magnitude;
     }
 
     #region Methods
@@ -68,7 +79,7 @@ public class MainCameraController : MonoBehaviour
     /// Player오브젝트의 FocusPoint오브젝트 Transform 반환 메서드
     /// </summary>
     /// <returns>Transform or Null</returns>
-    Transform GetFocusObject()
+    private Transform GetFocusObject()
     {
         GameObject player = GameObject.FindWithTag(TagName.Player);
         if (!player)
@@ -91,46 +102,39 @@ public class MainCameraController : MonoBehaviour
     /// <summary>
     /// 카메라 초기화 메서드
     /// </summary>
-    void InitCameraPosition()
+    private void InitCameraPosition()
     {
-        Vector3 initPosition = _focusObject.position + _focusObject.forward * -(Mathf.Abs(_initDistance));
+        Vector3 initPosition = _focusObject.position + -(_focusObject.forward) * (Mathf.Abs(_cameraDistance));
         transform.position = initPosition;
-        Vector3 defaultPoint = _focusObject.forward * -(Mathf.Abs(_defaultDistance));
-        _endPoint = defaultPoint;
+        _camera.fieldOfView = _initFOV;
+        _endPointPosition = transform.position - _focusObject.position;
+        _endPointFOV = _defaultFOV;
     }
 
     /// <summary>
-    /// 스크롤휠 확대/축소 메서드
+    /// 줌인/줌아웃 메서드
     /// </summary>
-    void Scroll()
+    private void ZoomInOut()
     {
         float wheelInput = Input.GetAxis("Mouse ScrollWheel");
         if (wheelInput == 0.0f)
         {
             return;
         }
-        float afterRadius = _radius + -(wheelInput * ScrollSensitivity);
-        Vector3 afterPoint = (transform.position - _focusObject.position).normalized * afterRadius;
-        _endPoint = afterPoint;
-    } 
-
+        float afterFOV = Mathf.Clamp(_camera.fieldOfView + -(wheelInput * ScrollSensitivity), MinFOV, MaxFOV);
+        _endPointFOV = afterFOV;
+    }
     #endregion
 
- 
-    /*
-    public void RotateAround()
+    public void dd()
     {
-        float a = Input.GetAxis("Mouse X");
-        Vector3 vector = gameObject.transform.position;
-        Quaternion quaternion = Quaternion.AngleAxis(a * 100 * Time.deltaTime + 0.0000001f, Vector3.up);
-        Vector3 vector2 = vector - _focusObject.position;
-        vector2 = quaternion * vector2;
-        FocusPoint = quaternion * FocusPoint;
-        //transform.position = Vector3.MoveTowards(transform.position, player.position, -(rotationDistance - Vector3.Distance(transform.position, player.position)));
-        if (Mathf.Abs((int)FocusPoint.x) < 3 && Mathf.Abs((int)FocusPoint.z) < 3 )
-        {
-            Debug.Log("In");
-        }
-    }*/
-    
+        float xMouseInput = Input.GetAxis("Mouse X") * MouseSensitivity * Time.deltaTime;
+        float yMouseInput = Input.GetAxis("Mouse Y") * MouseSensitivity * Time.deltaTime;
+
+        Quaternion quaternion = Quaternion.AngleAxis(xMouseInput, _focusObject.transform.up);
+        Quaternion quaternion1 = Quaternion.AngleAxis(-yMouseInput, _focusObject.transform.right);
+        Vector3 vector2 = quaternion * quaternion1 * _endPointPosition;
+
+        _endPointPosition = vector2;
+    }
 }

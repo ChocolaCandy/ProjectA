22021c0ac0ac8af9ed43fbe3ca94070415f1158d
ChocolaCandy <ChocolaCandy@users.noreply.github.com>
2024-05-09 23:49:29 +0900

    Merge test Camera Logic

--- Assets/Scripts/TestScript.cs
+++ Assets/Scripts/TestScript.cs
@@ -1,7 +1,4 @@
-using System;
-using System.Collections;
-using System.Security.Cryptography;
-using Unity.VisualScripting;
+
 using UnityEngine;
 
 public class TestScript : MonoBehaviour
@@ -14,14 +11,11 @@ public class TestScript : MonoBehaviour
     float max = 89.0f;
     public float value = 0.0f;
     public float _distance = 5.0f;
-    private void Start()
-    {
-        _endPointPosition = (transform.position - a.transform.position).normalized * _distance;
-        value = 0.0f;
-        transform.LookAt(a.transform);
-    }
+
     public float RotationSmoothTime = 0.12f;
     public bool isChanged = false;
+
+    public Camera _camera;
     //private void LateUpdate()
     //{
     //   /* cc();
@@ -68,9 +62,38 @@ public class TestScript : MonoBehaviour
     //    _endPointPosition = afterY;
     //}
 
+    bool adds = false;
+    Quaternion _endRotation = Quaternion.identity;
+    private void Start()
+    {
+        //Vector3 initPosition = a.transform.position + -(a.transform.forward) * (Mathf.Abs(_distance));
+        //transform.position = initPosition;
+        Vector3 dir = transform.position - a.transform.position;
+        _endPointPosition = dir.normalized;
+        _endRotation = Quaternion.LookRotation(-_endPointPosition);
+        _cinemachineTargetPitch = _endRotation.eulerAngles.y;
+        _cinemachiney = _endRotation.eulerAngles.x;
+        value = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+    }
+
     private void LateUpdate()
     {
+        Rotated();
+        //newdd();
+
+        transform.position = a.transform.position + _endPointPosition * _distance;
+        //if (Mathf.Abs(value) <= 89)
+        //    transform.LookAt(a.transform);
+
+    }
+
+    float _cinemachineTargetPitch = 0.0f;
+    float _cinemachiney = 0.0f;
+    public void Rotated()
+    {
         newaa();
+        newdd();
+        transform.rotation = Quaternion.Euler(_cinemachiney, _cinemachineTargetPitch, 0.0f);
     }
 
     public void newaa()
@@ -79,21 +102,24 @@ public class TestScript : MonoBehaviour
         if (xMouseInput == 0)
             return;
         Quaternion quaternion = Quaternion.AngleAxis(xMouseInput, a.transform.up);
-        Vector3 beforeVector = transform.position;
-        Vector3 afterVerctor = quaternion * beforeVector;
-        //float ats = Mathf.Atan2(transform.position.x - a.transform.position.y, (transform.position - a.transform.position).magnitude) * Mathf.Rad2Deg;
-        Vector3 vector2 = quaternion * _endPointPosition;
-        _endPointPosition = vector2;
+        Vector3 afterVerctor = quaternion * _endPointPosition;
+        _endPointPosition = afterVerctor;
+        _endRotation = quaternion * _endRotation;
+        _cinemachineTargetPitch += xMouseInput;
+        adds = true;
     }
 
-    public void dd()
+    public void newdd()
     {
         float yMouseInput = Input.GetAxis("Mouse Y") * 100 * Time.deltaTime;
+        yMouseInput = Mathf.Abs(Mathf.Clamp(value - yMouseInput, -90, 90)) == 90 ? 0 : yMouseInput;
         if (yMouseInput == 0)
             return;
         Quaternion quaternion1 = Quaternion.AngleAxis(-yMouseInput, transform.right);
         Vector3 afterY = quaternion1 * _endPointPosition;
-        value += -yMouseInput;
-        _endPointPosition = afterY;
+        _endPointPosition = afterY; ;
+        _endRotation = quaternion1 * _endRotation;
+        value -= yMouseInput;
+        adds = true;
     }
 }
